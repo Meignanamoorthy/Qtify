cd46c660cd1745cd16a4d3c9da08234991b0e359
Mei <meignanammoorthy.saravanan@halliburton.com>
2024-12-04 00:24:59 +0530

    carousel changes bugfix

--- src/utils/Button.js
+++ src/utils/Button.js
@@ -7,7 +7,19 @@ const Button = ({type, handleClick}) => {
                Give Feedback
             </button>
         )
-    } 
+    } else if(type === "collapse") {
+        return (
+            <button type="button" className="button collapse-button active" onClick={handleClick}>
+                Collapse
+            </button>
+        )
+    } else if(type === "showAll") {
+        return (
+            <button type="button" className="button collapse-button active" onClick={handleClick}>
+                Show All
+            </button>
+        )
+    }
     
 }
 
--- src/utils/Carousel.js
+++ src/utils/Carousel.js
@@ -4,13 +4,14 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import AlbumCard from "./../components/AlbumCard";
+import Button from './Button';
 
 const Carousel = ({heading, albums, handleToggle}) => {
     return (
         <Box padding="1rem" sx={{ mx: 1 }} md={{ mx: 3, my: 3 }}>
             <Stack direction="row" sx={{ justifyContent: "space-between" }}>
                 <h3 style={{color:"#fff"}} variant="h6" gutterBottom>{heading}</h3>
-                <h4 style={{color:"#34C94B", cursor: "pointer"}} onClick={handleToggle}>Show All</h4>
+                <Button style={{color:"#34C94B", cursor: "pointer"}} type="showAll" handleClick={handleToggle} />
             </Stack>
 
             <Swiper 
